c5265c6a1388031fd30cdaffb46898b688bd65a3
Elena Svyatoshenko <lena.svyatoshenko@mail.ru>
2023-10-30 12:19:47 +0300

    Update ListMeals.js

--- src/ListMeals.js
+++ src/ListMeals.js
@@ -1,13 +1,11 @@
 export const ListMeals = ({mealPlans, addMeal, deleteDay, selectedDayPlans, setSelectedDayPlans}) => {
   return (
     <div className="ListMeals_container">
-      <div>
         <h2 className="ListMeals_container-title">План питания на неделю</h2>
         <button className="btn_add" onClick={addMeal}>
           Добавить
         </button>
-      </div>
-      <div>
+      <div className="ListMeals_plans" >
         {mealPlans.map(({id, title, mealForADay}) => (
           <div key={id} className={`meal ${id === selectedDayPlans ? 'selectedColor' : 'defaultColor'}`}
           onClick={() => setSelectedDayPlans(id)}
