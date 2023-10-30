b12850a9a98514d9a357605da924cdc263853808
Elena Svyatoshenko <lena.svyatoshenko@mail.ru>
2023-10-30 11:33:46 +0300

    Update MealsAndIngredients.js

--- src/MealsAndIngredients.js
+++ src/MealsAndIngredients.js
@@ -7,15 +7,14 @@ export const MealsForm = ({selectedDayPlans, updateDay}) => {
     })
   }
 
-  if(!selectedDayPlans) return <p className="first-name-form">Мой план питания на неделю</p>// не показывать форму если 
-  //не нажата одна из категорий
+  if(!selectedDayPlans) return <p className="first-name-form">Мой план питания на неделю</p>
   return <div className="MealsAndIngrediets_container">
       <div className="MealsAndIngrediets_container-forms">
         <input
           type="text"
           className="inputForm"
           placeholder="День недели"
-          id="title" //соединяем id первого и второго компонента
+          id="title"
           value={selectedDayPlans.title}
           onChange={(e) => addInMyMealPlan("title", e.target.value)}
         />
@@ -23,7 +22,7 @@ export const MealsForm = ({selectedDayPlans, updateDay}) => {
       <textarea
         type="text" 
         placeholder="Мое меню на сегодня" 
-        id="mealForADay" //соединяем id первого и второго компонента
+        id="mealForADay"
         value={selectedDayPlans.mealForADay}
         onChange={(e) => addInMyMealPlan("mealForADay", e.target.value)}
       />
